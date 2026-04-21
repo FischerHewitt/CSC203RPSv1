6a30a89201b7b77528af76efdd77eaa3994321f7
fischerhewitt <fihewitt@calpoly.edu>
2026-04-20 21:51:58 -0700

    start of world

---
+++ src/World.java
@@ -0,0 +1,35 @@
+class World{
+    int width;
+    int height;
+    String[][] world;
+
+    World(int width, int height, int num_of_objects){//width is the size of the outer list, height is the size of the inner list
+        this.width = width;
+        this.height = height;
+        this.world = new String[width][height];
+
+        initialize_objects(num_of_objects);
+
+        }
+    }
+
+    public void initialize_objects(int num_of_objects){
+        for ()
+    }
+
+
+
+    addEntity():
+
+}
+
+/*
+world view
++-+-+-+-+
+| | | | |
++-+-+-+-+
+| | | | |
++-+-+-+-+
+| | | | |
++-+-+-+-+
+ */
